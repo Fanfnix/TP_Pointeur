5b9ce0200ed9360742460715e3062fb3760f56e8
Fanfnix <valentin@familledenis.net>
2025-01-17 14:06:51 +0100

    Setup struct

--- fct.h
+++ fct.h
@@ -1,6 +1,16 @@
 #ifndef FCT
 #define FCT
 
+struct Menu {
+    const char * option[20];
+};
+
+struct Application {
+    const char * nom[20];
+    struct Menu * menus;
+    int nb_menus;
+};
+
 void min_max(int tab[], int size, int * min, int * max);
 
 #endif
